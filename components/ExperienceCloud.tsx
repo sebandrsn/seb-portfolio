2fc74b2c4fdad1afc317e5e3934c6a8d6fdc748a
Sebastian Andersson <sebastiandersson@gmail.com>
2025-03-02 18:18:29 +0100

    feat: refac Experience interface

--- components/ExperienceCloud.tsx
+++ components/ExperienceCloud.tsx
@@ -3,79 +3,118 @@ import { FaLinkedin } from "react-icons/fa";
 
 interface Experience {
   id: string;
-  text?: string;
-  color?: string;
-  icon?: JSX.Element;
+  content: JSX.Element;
+  textColor?: string;
 }
 
 const experienceRows: Experience[][] = [
   [
-    { id: "C#", text: "C#", color: "text-gray-700" },
+    { id: "C#", content: <span>C#</span>, textColor: "text-gray-700" },
     {
       id: "Next",
-      text: "Next.js",
-      color: "text-gray-500",
-      icon: <div className="-mr-3 -ml-3">🔥</div>,
+      content: (
+        <span>
+          <span className="-mr-3 -ml-3">🔥</span>Next.js
+        </span>
+      ),
+      textColor: "text-gray-500",
     },
-
-    { id: "Mongo", text: "MongoDB", color: "text-gray-700" },
-    { id: "MVVM", text: "MVVM", color: "text-gray-500" },
-    { id: "MAP", text: "MicrosoftAzurePortal", color: "text-gray-700" },
+    { id: "Mongo", content: <span>MongoDB</span>, textColor: "text-gray-700" },
+    { id: "MVVM", content: <span>MVVM</span>, textColor: "text-gray-500" },
+    {
+      id: "MAP",
+      content: <span>MicrosoftAzurePortal</span>,
+      textColor: "text-gray-700",
+    },
   ],
   [
-    { id: "SQL", text: "SQL", color: "text-gray-500" },
-    { id: "JS", text: "Javascript", color: "text-gray-700" },
-    { id: "EC", text: "EdgeComputing", color: "text-gray-500" },
+    { id: "SQL", content: <span>SQL</span>, textColor: "text-gray-500" },
+    { id: "JS", content: <span>Javascript</span>, textColor: "text-gray-700" },
+    {
+      id: "EC",
+      content: <span>EdgeComputing</span>,
+      textColor: "text-gray-500",
+    },
     {
       id: "DOB",
-      text: "1989",
-      color: "text-gray-700",
-      icon: <div className="-mr-2 -ml-3">👶</div>,
+      content: (
+        <span>
+          <span className="-mr-3 -ml-3">👶</span>1989
+        </span>
+      ),
+      textColor: "text-gray-700",
     },
-    { id: "WPF", text: "WPF", color: "text-gray-500" },
+    { id: "WPF", content: <span>WPF</span>, textColor: "text-gray-500" },
   ],
   [
-    { id: "React", text: "React", color: "text-gray-700" },
-    { id: "Agile", text: "Agile", color: "text-gray-500" },
+    { id: "React", content: <span>React</span>, textColor: "text-gray-700" },
+    { id: "Agile", content: <span>Agile</span>, textColor: "text-gray-500" },
     {
       id: "Town",
-      text: "Östersund",
-      color: "text-gray-700",
-      icon: <div className="-mr-3 -ml-3">📍</div>,
+      content: (
+        <span>
+          <span className="-mr-4 -ml-4">📍</span>Östersund
+        </span>
+      ),
+      textColor: "text-gray-700",
     },
-    { id: "Fullstack", text: "Fullstack", color: "text-gray-500" },
-    { id: "AWS", text: "AWS", color: "text-gray-700" },
+    {
+      id: "Fullstack",
+      content: <span>Fullstack</span>,
+      textColor: "text-gray-500",
+    },
+    { id: "AWS", content: <span>AWS</span>, textColor: "text-gray-700" },
   ],
   [
     {
       id: "EMail",
-      text: "sebastiandersson@gmail.com",
-      color: "text-gray-500",
-      icon: <div>✉️</div>,
+      content: (
+        <span>
+          <span>✉️</span>sebastiandersson@gmail.com
+        </span>
+      ),
+      textColor: "text-gray-500",
     },
   ],
   [
-    { id: "ADOS", text: "AzureDevOpsServices", color: "text-gray-700" },
-    { id: "Angular", text: "Angular", color: "text-gray-500" },
+    {
+      id: "ADOS",
+      content: <span>AzureDevOpsServices</span>,
+      textColor: "text-gray-700",
+    },
+    {
+      id: "Angular",
+      content: <span>Angular</span>,
+      textColor: "text-gray-500",
+    },
   ],
-
   [
-    { id: "NET", text: ".NET", color: "text-gray-500" },
-    { id: "Git", text: "Git", color: "text-gray-700" },
-    { id: "Fastly", text: "Fastly", color: "text-gray-500" },
+    { id: "NET", content: <span>.NET</span>, textColor: "text-gray-500" },
+    { id: "Git", content: <span>Git</span>, textColor: "text-gray-700" },
+    { id: "Fastly", content: <span>Fastly</span>, textColor: "text-gray-500" },
     {
       id: "LinkedIn",
-      text: "LinkedIn",
-      color: "text-gray-700",
-      icon: <FaLinkedin className="text-blue-900" />,
+      content: (
+        <span className="flex flex-row">
+          <FaLinkedin className="text-blue-900" />
+          LinkedIn
+        </span>
+      ),
+      textColor: "text-gray-700",
     },
   ],
   [
-    { id: "TS", text: "Typescript", color: "text-gray-700" },
-    { id: "TL", text: "TeamLead", color: "text-gray-500" },
+    { id: "TS", content: <span>Typescript</span>, textColor: "text-gray-700" },
+    { id: "TL", content: <span>TeamLead</span>, textColor: "text-gray-500" },
   ],
-  [{ id: "Tailwind", text: "Tailwindcss", color: "text-gray-500" }],
-  [{ id: "Rocket", icon: <div>🚀</div> }],
+  [
+    {
+      id: "Tailwind",
+      content: <span>Tailwindcss</span>,
+      textColor: "text-gray-500",
+    },
+  ],
+  [{ id: "Rocket", content: <span>🚀</span> }],
 ];
 
 export default function ExperienceCloud() {
@@ -84,9 +123,8 @@ export default function ExperienceCloud() {
       {experienceRows.map((row, rowIndex) => (
         <div key={rowIndex} className="flex flex-wrap text-6xl font-bold">
           {row.map((exp) => (
-            <div key={exp.id} className={`flex ${exp.color}`}>
-              {exp.icon && <span className="-mr-1">{exp.icon}</span>}
-              {exp.text}
+            <div key={exp.id} className={`flex ${exp.textColor}`}>
+              {exp.content}
             </div>
           ))}
         </div>
