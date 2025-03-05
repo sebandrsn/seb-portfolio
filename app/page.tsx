f9940d1ffab09433ef23cb2b177057ab357339a7
Sebastian Andersson <sebastiandersson@gmail.com>
2025-03-05 17:16:41 +0100

    feat: change image

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,28 @@ import ExperienceCloud from "@/components/ExperienceCloud";
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import { motion, useScroll, useTransform } from "motion/react";
 import Profile from "@/components/Profile";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import Link from "next/link";
 
 export default function Home() {
   const { scrollY } = useScroll();
-  const headerRef = useRef<HTMLHeadingElement>(null);
+  const nameRef = useRef<HTMLHeadingElement>(null);
   const thisIsMeRef = useRef<HTMLHeadingElement>(null);
   const [initialOffset, setInitialOffset] = useState<number | null>(null);
-  const [thisIsMeMargin, setThisIsMeMargin] = useState(0);
+  const [thisIsMeMarginLeft, setThisIsMeMarginLeft] = useState(0);
 
   useLayoutEffect(() => {
-    if (headerRef.current && thisIsMeRef.current) {
+    if (nameRef.current && thisIsMeRef.current) {
+      const headerHeight = nameRef.current.offsetHeight;
+      setInitialOffset(window.innerHeight - headerHeight);
+
       const thisIsMeMargin =
         window.innerWidth -
-        headerRef.current.offsetWidth -
+        nameRef.current.offsetWidth -
         thisIsMeRef.current.offsetWidth -
         40;
-      setThisIsMeMargin(thisIsMeMargin);
-    }
-  }, []);
-
-  useLayoutEffect(() => {
-    if (headerRef.current) {
-      const headerHeight = headerRef.current.offsetHeight;
-      setInitialOffset(window.innerHeight - headerHeight);
+      setThisIsMeMarginLeft(thisIsMeMargin);
     }
   }, []);
 
@@ -37,45 +36,63 @@ export default function Home() {
     initialOffset !== null ? [initialOffset, 0] : [0, 0],
     { clamp: true },
   );
-  const cloudY = useTransform(scrollY, [0, 1000], ["0vh", "-100vh"]);
+
   const thisIsMeX = useTransform(
     scrollY,
     initialOffset !== null ? [0, initialOffset] : [0, 1],
     ["-250vh", "0vh"],
     { clamp: true },
   );
+
   const meY = useTransform(scrollY, [0, 500], ["100vh", "0vh"]);
+
+  const opacity = useTransform(scrollY, [200, 50], [1, 0]);
 
   return (
     <div>
       <motion.h1
-        className="fixed right-2 bg-gradient-to-r from-pink-600 via-indigo-500 to-teal-300 bg-clip-text text-6xl font-bold text-transparent md:text-9xl"
+        className="fixed right-2 z-10 bg-gradient-to-r from-pink-600 via-indigo-500 to-teal-300 bg-clip-text text-6xl font-bold text-transparent md:text-9xl"
         style={{
           top: headerY,
           visibility: initialOffset !== null ? "visible" : "hidden",
         }}
       >
-        <span ref={headerRef}>sebastian</span>
+        <span ref={nameRef}>sebastian</span>
+        <motion.div
+          className="flex flex-row justify-end space-x-4 text-6xl"
+          style={{ opacity }}
+        >
+          <Link
+            href="https://github.com/sebandrsn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoGithub className="text-white" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/sebastian-andersson-981641138"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-blue-900" />
+          </Link>
+        </motion.div>
       </motion.h1>
       <motion.div
         className="fixed top-0 text-6xl font-bold md:text-9xl"
-        style={{ x: thisIsMeX, marginLeft: thisIsMeMargin }}
+        style={{ x: thisIsMeX, marginLeft: thisIsMeMarginLeft }}
       >
         <span ref={thisIsMeRef}>this is me:</span>
       </motion.div>
-      <div className="min-h-screen cursor-default">
-        <motion.div style={{ y: cloudY }}>
-          <ExperienceCloud />
-        </motion.div>
+      <div className="sticky top-0 min-h-screen cursor-default">
+        <ExperienceCloud />
       </div>
       <div className="min-h-screen">
         <motion.div style={{ y: meY }}>
           <Profile />
         </motion.div>
       </div>
-      <div className="min-h-screen">a</div>
-      <div className="min-h-screen">b</div>
-      <div className="min-h-screen">c</div>
+
       <div className="flex justify-center">
         <MdOutlineKeyboardDoubleArrowDown className="fixed bottom-0 animate-bounce text-6xl text-gray-500 opacity-50" />
       </div>
--- components/ExperienceCloud.tsx
+++ components/ExperienceCloud.tsx
@@ -1,147 +1,167 @@
+"use client";
+
+import { useScroll, useTransform, motion, MotionValue } from "motion/react";
+import Link from "next/link";
 import { JSX } from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { IoLogoGithub } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 
 interface Experience {
   id: string;
-  content: JSX.Element;
+  content: string;
   textColor?: string;
+  unicodeIcon?: JSX.Element;
+  opacity?: MotionValue<number>;
+  url?: string;
 }
 
 const experienceRows: Experience[][] = [
   [
-    { id: "C#", content: <span>C#</span>, textColor: "text-gray-700" },
-    {
-      id: "Next",
-      content: (
-        <span>
-          <span className="-mr-3 -ml-3">🔥</span>Next.js
-        </span>
-      ),
-      textColor: "text-gray-500",
-    },
-    { id: "Mongo", content: <span>MongoDB</span>, textColor: "text-gray-700" },
-    { id: "MVVM", content: <span>MVVM</span>, textColor: "text-gray-500" },
-    {
-      id: "MAP",
-      content: <span>MicrosoftAzurePortal</span>,
-      textColor: "text-gray-700",
-    },
+    { id: "C#", content: "C#", textColor: "text-gray-700" },
+    { id: "Next", content: "🔥Next.js", textColor: "text-gray-500" },
+    { id: "Mongo", content: "MongoDB", textColor: "text-gray-700" },
+    { id: "MVVM", content: "MVVM", textColor: "text-gray-500" },
+    { id: "MAP", content: "MicrosoftAzurePortal", textColor: "text-gray-700" },
   ],
   [
-    { id: "SQL", content: <span>SQL</span>, textColor: "text-gray-500" },
-    { id: "JS", content: <span>Javascript</span>, textColor: "text-gray-700" },
-    {
-      id: "EC",
-      content: <span>EdgeComputing</span>,
-      textColor: "text-gray-500",
-    },
-    {
-      id: "DOB",
-      content: (
-        <span>
-          <span className="-mr-3 -ml-3">👶</span>1989
-        </span>
-      ),
-      textColor: "text-gray-700",
-    },
-    { id: "WPF", content: <span>WPF</span>, textColor: "text-gray-500" },
+    { id: "SQL", content: "SQL", textColor: "text-gray-500" },
+    { id: "JS", content: "Javascript", textColor: "text-gray-700" },
+    { id: "EC", content: "EdgeComputing", textColor: "text-gray-500" },
+    { id: "DOB", content: "👶1989", textColor: "text-gray-700" },
+    { id: "WPF", content: "WPF", textColor: "text-gray-500" },
   ],
   [
-    { id: "React", content: <span>React</span>, textColor: "text-gray-700" },
-    { id: "Agile", content: <span>Agile</span>, textColor: "text-gray-500" },
-    {
-      id: "Town",
-      content: (
-        <span>
-          <span className="-mr-4 -ml-4">📍</span>Östersund
-        </span>
-      ),
-      textColor: "text-gray-700",
-    },
-    {
-      id: "Fullstack",
-      content: <span>Fullstack</span>,
-      textColor: "text-gray-500",
-    },
-    { id: "AWS", content: <span>AWS</span>, textColor: "text-gray-700" },
+    { id: "React", content: "React", textColor: "text-gray-700" },
+    { id: "Agile", content: "Agile", textColor: "text-gray-500" },
+    { id: "Town", content: "📍Östersund", textColor: "text-gray-700" },
+    { id: "Fullstack", content: "Fullstack", textColor: "text-gray-500" },
+    { id: "AWS", content: "AWS", textColor: "text-gray-700" },
   ],
   [
     {
       id: "EMail",
-      content: (
-        <span>
-          <span>✉️</span>sebastiandersson@gmail.com
-        </span>
-      ),
+      content: "sebastiandersson@gmail.com",
       textColor: "text-gray-500",
+      unicodeIcon: <MdEmail className="text-white" />,
     },
   ],
   [
-    {
-      id: "ADOS",
-      content: <span>AzureDevOpsServices</span>,
-      textColor: "text-gray-700",
-    },
-    {
-      id: "Angular",
-      content: <span>Angular</span>,
-      textColor: "text-gray-500",
-    },
+    { id: "ADOS", content: "Azure DevOpsServices", textColor: "text-gray-700" },
+    { id: "Angular", content: "Angular", textColor: "text-gray-500" },
   ],
   [
-    { id: "NET", content: <span>.NET</span>, textColor: "text-gray-500" },
-    { id: "Git", content: <span>Git</span>, textColor: "text-gray-700" },
-    { id: "Fastly", content: <span>Fastly</span>, textColor: "text-gray-500" },
+    { id: "NET", content: ".NET", textColor: "text-gray-500" },
+    { id: "Git", content: "Git", textColor: "text-gray-700" },
+    { id: "Fastly", content: "Fastly", textColor: "text-gray-500" },
     {
       id: "LinkedIn",
-      content: (
-        <span className="flex flex-row">
-          <FaLinkedin className="text-blue-900" />
-          LinkedIn
-        </span>
-      ),
+      content: "LinkedIn",
       textColor: "text-gray-700",
+      unicodeIcon: <FaLinkedin className="text-blue-900" />,
+      url: "https://www.linkedin.com/in/sebastian-andersson-981641138",
     },
   ],
   [
-    { id: "TS", content: <span>Typescript</span>, textColor: "text-gray-700" },
-    { id: "TL", content: <span>TeamLead</span>, textColor: "text-gray-500" },
+    { id: "TS", content: "Typescript", textColor: "text-gray-700" },
+    { id: "TL", content: "TeamLead", textColor: "text-gray-500" },
   ],
-  [
-    {
-      id: "Tailwind",
-      content: <span>Tailwindcss</span>,
-      textColor: "text-gray-500",
-    },
-  ],
+  [{ id: "Tailwind", content: "Tailwindcss", textColor: "text-gray-500" }],
   [
     {
       id: "GitHub",
-      content: (
-        <span className="flex flex-row">
-          <IoLogoGithub className="text-white" />
-          GitHub
-        </span>
-      ),
+      content: "GitHub",
       textColor: "text-gray-700",
+      unicodeIcon: <IoLogoGithub className="text-white" />,
+      url: "https://github.com/sebandrsn",
     },
   ],
-  [{ id: "Rocket", content: <span>🚀</span> }],
 ];
 
 export default function ExperienceCloud() {
+  const { scrollY } = useScroll();
+
+  const randomOffset = Math.random() * 200;
+  const opacity = useTransform(
+    scrollY,
+    [randomOffset, randomOffset + 200],
+    [1, 0],
+  );
+
+  const rocketRotate = useTransform(scrollY, [0, 300], [0, -45]);
+  const rocketY = useTransform(scrollY, [300, 1000], ["0vh", "-100vh"]);
+
   return (
     <div className="space-y-4 opacity-50">
-      {experienceRows.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex flex-wrap text-6xl font-bold">
-          {row.map((exp) => (
-            <div key={exp.id} className={`flex ${exp.textColor}`}>
-              {exp.content}
+      {experienceRows.map((row) => (
+        <div
+          key={row.map((exp) => exp.id).join("-")}
+          className="flex flex-wrap text-6xl font-bold"
+        >
+          {row.map((exp, expIndex) => (
+            <div key={`${exp.id}-${expIndex}`} className="flex flex-row">
+              {exp.unicodeIcon && (
+                <motion.div style={{ opacity }}>
+                  {exp.url !== undefined ? (
+                    <Link
+                      href={exp.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {exp.unicodeIcon}
+                    </Link>
+                  ) : (
+                    <>{exp.unicodeIcon}</>
+                  )}
+                </motion.div>
+              )}
+              <div className={`flex ${exp.textColor}`}>
+                {typeof exp.content === "string" ? (
+                  Array.from(exp.content).map((char, index) => {
+                    const randomOffset = Math.random() * 200;
+                    const opacity = useTransform(
+                      scrollY,
+                      [randomOffset, randomOffset + 200],
+                      [1, 0],
+                    );
+
+                    return (
+                      <motion.span
+                        key={`${exp.id}-${index}`}
+                        style={{ opacity }}
+                      >
+                        {exp.url !== undefined ? (
+                          <Link
+                            href={exp.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {char}
+                          </Link>
+                        ) : (
+                          <>{char}</>
+                        )}
+                      </motion.span>
+                    );
+                  })
+                ) : (
+                  <motion.div style={{ opacity }}>{exp.content}</motion.div>
+                )}
+              </div>
             </div>
           ))}
         </div>
       ))}
+      <motion.div
+        className="inline-flex text-6xl"
+        style={{
+          rotate: rocketRotate,
+          y: rocketY,
+          transformOrigin: "center center",
+        }}
+      >
+        🚀
+      </motion.div>
     </div>
   );
 }
