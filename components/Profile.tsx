469fe98e8c67853a9af30f07996f8c4fe25befb0
Sebastian Andersson <sebastiandersson@gmail.com>
2025-03-10 21:52:13 +0100

    feat(profile): set width on image depending on device

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,10 +6,10 @@ export default function Profile() {
       <div className="flex flex-col items-center space-x-4 lg:flex-row">
         <Image
           src="/images/me.jpg"
-          alt="Sebastian Andersson a hat, wearing sunglasses and has a frozen mustache."
+          alt="Sebastian Andersson wearing a hat, sunglasses, and has a frozen mustache."
           width={350}
           height={467}
-          className="rounded-2xl"
+          className="h-auto max-w-[80%] rounded-2xl md:max-w-[50%] lg:max-w-[350px]"
         />
         <div>
           <h2 className="text-xl font-bold">Sebastian Andersson</h2>
