6f7f5a610d3956da843d46547adc331c050a524a
Sebastian Andersson <sebastiandersson@gmail.com>
2025-04-05 19:57:29 +0200

    feat: adds some space top

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             className="h-auto"
           />
 
-          <div className="absolute top-56 h-32 w-56 px-2 font-mono text-sm leading-tight text-green-400 sm:text-base">
+          <div className="absolute top-60 h-32 w-56 px-2 font-mono text-sm leading-tight text-green-400 sm:text-base">
             <div className="break-words whitespace-pre-wrap">
               <TypewriterText />
             </div>
